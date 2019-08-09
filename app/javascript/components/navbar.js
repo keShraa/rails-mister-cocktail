9cd511613349faa87247034826049e645bf0a147
julien legay <legayj@gmail.com>
2019-08-09 11:55:26 +0200

    Add back buttons

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -4,7 +4,6 @@ const initUpdateNavbarOnScroll = () => {
     window.addEventListener('scroll', () => {
       if (window.scrollY >= window.innerHeight / 3) {
         navbar.classList.add('navbar-lewagon-white');
-        console.log("white wesh")
       } else {
         navbar.classList.remove('navbar-lewagon-white');
       }
