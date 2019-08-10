12b7e2a3fa3effcb102162b50feb1be66492206b
julien legay <legayj@gmail.com>
2019-08-10 14:07:44 +0200

    Update FRONT for forms and adding delete option

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # List of cocktails
   # See details of a given cocktail, with the dose needed for each ingredient
   # Create a new cocktail
-  resources :cocktails, only: %i[index show new create] do
+  resources :cocktails, only: %i[index show new create destroy] do
     resources :doses, only: %i[new create]
   end
 
